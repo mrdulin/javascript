1399835a8795cce3b937b26605ae1c10b27b2e6c
novaline <novaline@qq.com>
2016-12-11 23:29:13 +0800

    add others demo

---
+++ others/输出100~999之间的水仙花数.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Number} min
+ * @param {Number} max
+ * @returns {Array} 
+ */
+var narcissistic = function(min, max) {
+    var numArr = [], i = min, sum, result = [];
+    for(; i < max; ++i) {
+        sum = 0;
+        numArr = i.toString().split('');
+        numArr.forEach(function(num) {
+            num = parseInt(num, 10);
+            sum += Math.pow(num, 3);
+        });
+
+        if(i === sum) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+console.log(narcissistic(100, 999));
