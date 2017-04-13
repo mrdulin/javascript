f5885efbcc91666330ec46eb6df927cad3deff4f
mrdulin <novaline@qq.com>
2017-04-13 19:36:15 +0800

    add some demos

---
+++ demos/array-intersection.js
@@ -0,0 +1,6 @@
+function intersection(a1, a2) {
+}
+
+const a = [1, 2, 3, 4];
+const b = [1, 2];
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
- 
+
 gulp.task('webserver', function() {
   gulp.src('demos')
     .pipe(webserver({
+      host: '0.0.0.0',
       livereload: true,
       open: true,
       path: '/'
