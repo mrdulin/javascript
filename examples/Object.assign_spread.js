dbe3b55cf22555b88edc911ffc52000cd0acc9b3
novaline <novaline@qq.com>
2017-09-07 09:47:09 +0800

    update

--- examples/Object.assign_spread.js
+++ examples/Object.assign_spread.js
@@ -1,9 +1,9 @@
-let a = {name: 'dulin'}
+let a = { name: 'dulin' }
 
 let b = Object.assign(
   {},
   a,
-  {name: 'novaline'}
+  { name: 'novaline' }
 )
 
 console.log(b); //{"name":"novaline"}
@@ -28,11 +28,31 @@ let m = Object.assign(
 console.log(m)  //{"pagination":{"pageIndex":2}}
 
 let n = {
-    ...x,
-    pagination: {
-      ...x.pagination,
-      pageIndex: 10
-    }
+  ...x,
+  pagination: {
+    ...x.pagination,
+    pageIndex: 10
+  }
 }
 
 console.log(n)  //{"pagination":{"pageIndex":10,"pageSize":10}}
+
+const state = {
+  books: [
+    { id: 1, name: 'react' },
+    { id: 2, name: 'angular' },
+    { id: 3, name: 'rxjs' }
+  ],
+  names: ['react', 'angular', 'rxjs']
+};
+
+const newBooks = state.books.map((book) => {
+  if (book.id === 1) {
+    return Object.assign({}, book, { name: 'jquery' });
+  } else {
+    return book;
+  }
+});
+
+const nextState = Object.assign({}, state, { books: newBooks });
+console.log(nextState);
