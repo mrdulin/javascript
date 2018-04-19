8b34345ff6f2d5a3384d8ba05b49e47fc88780a0
mrdulin <novaline@qq.com>
2018-04-19 16:59:39 +0800

    format code

--- demos/loop-performance.js
+++ demos/loop-performance.js
@@ -1,41 +1,42 @@
 var len = 100000000;
 
 function test(fn, message) {
-    console.time(message);
-    fn();
-    console.timeEnd(message);
+  console.time(message);
+  fn();
+  console.timeEnd(message);
 }
 
 test(function() {
-    var b;
-    for(var i = len; i--;) {
-        b = i;
-    }
-    console.log(b);
+  var b;
+  for (var i = len; i--; ) {
+    b = i;
+  }
+  console.log(b);
 }, 'for-loop optimize');
 
 test(function() {
-    var a;
-    for(var i = 0; i < len; i ++) {
-        a = i;
-    }
-    console.log(a);
+  var a;
+  for (var i = 0; i < len; i++) {
+    a = i;
+  }
+  console.log(a);
 }, 'for-loop non-optimize');
 
 test(function() {
-    var j = len, c;
-    while(j--) {
-        c = j;
-    }
-    console.log(c);
+  var j = len,
+    c;
+  while (j--) {
+    c = j;
+  }
+  console.log(c);
 }, 'while-loop optimize');
 
-
 test(function() {
-    var k = 0, d;
-    while(k < len) {
-        d = k;
-        k++;
-    }
-    console.log(d);
+  var k = 0,
+    d;
+  while (k < len) {
+    d = k;
+    k++;
+  }
+  console.log(d);
 }, 'while-loop non-optimize');
--- demos/loopBlockingTimeout.js
+++ demos/loopBlockingTimeout.js
@@ -1,10 +1,9 @@
 const start = Date.now();
 setTimeout(() => {
-    const end = Date.now();
-    console.log('Time elapsed:', end - start, 'ms');
+  const end = Date.now();
+  console.log('Time elapsed:', end - start, 'ms');
 }, 500);
-while(Date.now() - start < 1000) {};
-
+while (Date.now() - start < 1000) {}
 
 //结果
 //Time elapsed: 1000 ms
